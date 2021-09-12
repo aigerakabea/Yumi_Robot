55054d86793716ca574fe2ce02d29b49a086af83
Rohithbotusers <90542170+Rohithbotusers@users.noreply.github.com>
2021-09-12 11:42:04 +0530

    Update Alive.py

--- EmiliaAnimeBot/modules/Alive.py
+++ EmiliaAnimeBot/modules/Alive.py
@@ -7,10 +7,10 @@ PHOTO = "https://telegra.ph/file/d7d927fe899b7415e21d7.jpg"
 @register(pattern=("/alive"))
 async def awake(event):
   PIKACHU = event.sender.first_name
-  PIKACHU = "**✗ ɪ'ᴀᴍ 👸ʏᴜᴍɪ ✗** \n\n"
-  PIKACHU += "**✗ ɪ'ᴀᴍ 👸ǫᴜᴇᴇɴ ᴏғ〘 ᴛɢ 〙**\n\n"
+  PIKACHU = "**✗ ɪ'ᴀᴍ VALT AOI ✗** \n\n"
+  PIKACHU += "**✗ ɪ'ᴀᴍ beyblader bot for your group**\n\n"
   PIKACHU += "**✗ ɪ'ᴀᴍ ʜᴀᴠᴇ ᴘᴏᴡᴇʀ ғᴜʟʟ Cᴏᴍᴍᴇɴᴅs ᴡɪᴛʜ ./help**\n\n"
-  PIKACHU += "**✗ Mʏ ʟᴇɢᴇɴᴅ ɪ ᴍᴇᴀɴ ᴍʏ ᴏᴡɴᴇʀ 🤗:** [✗ᴄᴛᴢғᴀᴍɪʟʏ✗](https://t.me/ctzfamily)\n\n"
+  PIKACHU += "**✗ Mʏ ʟᴇɢᴇɴᴅ ɪ ᴍᴇᴀɴ ᴍʏ ᴏᴡɴᴇʀ 🤗:** [✗Rohith✗](https://t.me/Rohith_no-1)\n\n"
   PIKACHU += "**♡ Python Version : 3.9**\n\n"
   BUTTON = [[Button.url("𝙎𝙐𝙋𝙋𝙊𝙍𝙏", "https://t.me/PigasusSupport"), Button.url("𝙐𝙋𝘿𝘼𝙏𝙀", "https://t.me/PigasusUpdates")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=PIKACHU,  buttons=BUTTON)
